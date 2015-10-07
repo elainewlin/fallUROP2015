2ba8ee400f55dd6d2c2e28e8d34b822fbe3068cc
Elaine Lin <elin1@mit.edu>
2015-10-07 10:18:43 -0400

    Google search code

---
+++ classifier.py
@@ -0,0 +1,9 @@
+import sklearn
+import redis
+f = open('dump.rdb','r')
+print f.
+#feature: title contains "meta-analysis"
+
+#feature: title contains "breast cancer"
+
+#feature: abstract contains "meta-analysis"
